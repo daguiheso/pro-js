7fa9b8202470cc849007c09a14a8e8ae95c7657c
Daniel H <daguiheso@gmail.com>
2019-09-13 10:43:34 -0500

    proxy js

---
+++ assets/js/proxy.js
@@ -0,0 +1,23 @@
+const target = {
+  red: 'rojo',
+  green: 'verde',
+  yellow: 'amarillo'
+}
+
+const handler = {
+  get(obj, prop) {
+    if(prop in obj) {
+      return obj[prop]
+    }
+
+    const suggestion = Object.keys(obj).find(key => {
+      return Levenshtein.get(key, prop) < 3
+    })
+
+    if(suggestion) console.log(`${prop} no se encontro. Quisiste decir: ${suggestion}`)
+
+    return obj[prop]
+  }
+}
+
+const p = new Proxy(target, handler)
